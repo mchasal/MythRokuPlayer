e2e00b39251ffe3729e637c2a26dbed41f0c154a
Michael Chase-Salerno <mcs@chasal.net>
2010-12-11 11:07:20 -0500

    Fix thumbnails

--- mythroku/rokuencode.sh
+++ mythroku/rokuencode.sh
@@ -14,6 +14,11 @@ newname="$MYTHDIR/$newbname.mp4"
 #/usr/bin/HandBrakeCLI -i $1/$2 -o $newname -e x264 -b 1500 -E faac -B 256 -R 48 -w 720
 /usr/bin/HandBrakeCLI -i $MYTHDIR/$MPGFILE -o $newname -e x264 -b 1500 -E faac -B 256 -R 48
 
+#Mythtv seems to have problems with keyframes in mp4s, so make previews with ffmpeg
+ffmpeg -ss 34 -vframes 1 -i $newname -y -f image2  $MYTHDIR/$newbname.mp4.png
+ffmpeg -ss 34 -vframes 1 -i $newname -y -f image2 -s 100x75 $MYTHDIR/$newbname.mp4.64.100x75.png
+ffmpeg -ss 34 -vframes 1 -i $newname -y -f image2 -s 320x240 $MYTHDIR/$newbname.mp4.64.320x240.png
+
 # remove the orignal mpg and update the db to point to the mp4
 NEWFILESIZE=`du -b "$newname" | cut -f1`
 echo "UPDATE recorded SET basename='$newbname.mp4',filesize='$NEWFILESIZE',transcoded='1' WHERE basename='$2';" > /tmp/update-database.sql
